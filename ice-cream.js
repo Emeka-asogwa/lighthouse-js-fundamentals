4f156e91ee0cc831bec9148d406f37bb6e0485d5
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-05-07 19:35:28 +0000

    Practice exercise

---
+++ ice-cream.js
@@ -0,0 +1,8 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
+console.log(iceCreamFlavours);
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0])   /// first flavour of the iceCream
+console.log(iceCreamFlavours[iceCreamFlavours.length-1]); // The last flavour of the iceCream
+console.log(iceCreamFlavours.length);  // The total number of iceCream
